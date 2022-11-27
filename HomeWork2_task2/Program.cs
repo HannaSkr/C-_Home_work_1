1f411cc8fc5ed4c04c576e260d67ef3c04ca4f87
Hanna <skrane4ka@gmail.com>
2022-11-27 23:25:51 +0100

    +

--- HomeWork2_task2/Program.cs
+++ HomeWork2_task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+Random pkp = new Random();
+int number = pkp.Next(); //любое число с любой разрядностью
+
+Console.WriteLine($"{number}");
+
+int digitIndex = 2; //индекс третьей цифры
+
+if(number >  99)
+{
+    int digit = number.ToString()[digitIndex] - '0';
+    Console.WriteLine(digit);
+} else {
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
