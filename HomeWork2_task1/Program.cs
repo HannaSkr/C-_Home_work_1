fb98b35bb4951e0c993cbe103a83f35d3f040036
Hanna <skrane4ka@gmail.com>
2022-11-27 22:34:54 +0100

    +

--- HomeWork2_task1/Program.cs
+++ HomeWork2_task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+Random pkp = new Random();
+int number = pkp.Next(100, 1000);
+
+Console.WriteLine($"{number}");
+
+int secondDigit = number / 10 % 10;
+Console.WriteLine($"Вторая цифра числа {secondDigit}");
