ab356765a5c0e5c3748fe28a2d202b97f9d640ad
Hanna <skrane4ka@gmail.com>
2022-11-29 10:12:30 +0100

    done

--- HomeWork3_task3/Program.cs
+++ HomeWork3_task3/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+int number1 = GetNumber("Введите число");
+for(int i = 1; i <= number1; i++)
+{
+    Console.Write($" {Math.Pow(i, 3)}, ");
+}
