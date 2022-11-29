17c049f02f695e8a0f100daaea4cbf8066ddbf22
Hanna <skrane4ka@gmail.com>
2022-11-29 09:19:27 +0100

    first version done

--- HomeWork3_task1/Program.cs
+++ HomeWork3_task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Введите пятизначное число");
+string x1 = Console.ReadLine();
+char[] arr = x1.ToCharArray();
+Array.Reverse(arr);
+string x2 = new String(arr);
+ 
+if (x1 == x2) Console.WriteLine("Палиндром");
+ else Console.WriteLine("Не палиндром");
+
