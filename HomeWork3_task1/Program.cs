3612e6c08ee74222f085d42da48fd755833b2733
Hanna <skrane4ka@gmail.com>
2022-11-29 10:33:28 +0100

    доточила

--- HomeWork3_task1/Program.cs
+++ HomeWork3_task1/Program.cs
@@ -4,9 +4,31 @@
 14212 -> нет
 12821 -> да
 23432 -> да*/
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
 
-Console.WriteLine("Введите пятизначное число");
-string x1 = Console.ReadLine();
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 10000)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели НЕ пятизначное число. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+int numberFirst = GetNumber("Введите пятизначное число");
+string x1 = Convert.ToString(numberFirst);
 char[] arr = x1.ToCharArray();
 Array.Reverse(arr);
 string x2 = new String(arr);
