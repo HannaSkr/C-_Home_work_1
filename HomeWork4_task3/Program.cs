ef87d7d30c846f3b2219e10f7f9c9f8d5406731d
Hanna <skrane4ka@gmail.com>
2022-12-02 13:55:33 +0100

    first version

---
+++ HomeWork4_task3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+
+Console.Write("Введите количество элементов массива:\t");
+int elementsNumber = int.Parse(Console.ReadLine());
+int[] myArray = new int[elementsNumber];
+
+for(int i = 0; i < myArray.Length; i++)
+{
+        myArray[i] = new Random().Next(0, myArray.Length);
+        Console.Write($"{myArray[i]} ");
+}
+
