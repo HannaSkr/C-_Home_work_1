87d117bff7413af0506b942ca7dc91aed3879f07
Hanna <skrane4ka@gmail.com>
2022-12-06 15:12:20 +0100

    add task with count of even numbers

---
+++ HomeWork5_task1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+int[] GetArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(100,1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int GetEvenNumbers(int[] array)
+{
+    int countEvenNumbers = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) countEvenNumbers += 1;
+    }
+    return countEvenNumbers;
+}
+
+Console.WriteLine("Ведите размер массива");
+int[] array = GetArray(int.Parse(Console.ReadLine()?? ""));
+
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел в массиве: {GetEvenNumbers(array)}");
