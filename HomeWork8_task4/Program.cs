5060a4d42da8aab26ac337910bfbe61a8fe0ec72
Hanna <skrane4ka@gmail.com>
2022-12-16 22:52:13 +0100

    3-size matrix

---
+++ HomeWork8_task4/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)*/
+
+//получить числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+//задать матрицу и заполнить ее числами
+int[,,] GetMatrix(int m, int n, int o)
+{
+    int[,,] matrix = new int[m,n,o];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = rnd.Next(10, 100);
+            }            
+        }
+    }
+    return matrix;
+}
+
+//распечатать матрицу
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]} ({i}, {j}, {k}) ");
+            }
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите размерность первого измерения:");
+int n = GetNumber("Введите размерность второго измерения");
+int o = GetNumber("Введите размерность третьего измерения");
+int[,,] matrix = GetMatrix(m,n,o);
+PrintMatrix(matrix);
