e2f4a62ad3eba1d94e177461db585c78fe0e42c8
Hanna <skrane4ka@gmail.com>
2022-12-21 20:23:54 +0100

    add sum of numbers from interval

---
+++ HomeWork9_task2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+//получить числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+int SumInterval (int n, int m)
+{
+    if (n > m)
+    {
+        if (n == m) return n;
+        return SumInterval(n-1, m) + n;
+    } else {
+        if (n == m) return n;
+        return SumInterval(n-1, m) + n;
+    }     
+}
+  
+int m = GetNumber("Введите число больше 0");
+int n = GetNumber("Введите число больше 0");
+int resultSumm = SumInterval(n, m);
+Console.WriteLine($"Сумма чисел в интервале от  {m}  до {n} равна  {resultSumm}");
